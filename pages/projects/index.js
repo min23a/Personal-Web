fcf777183e309ebb1e7376cf976051231b17c00e
min23a <minhazuloabedin0611@gmail.com>
2026-01-27 00:33:54 +0100

    started seo in projects also for indexing

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,9 +3,11 @@ import Layout from '@/Components/Layout'
 import Ending from '@/Components/Section/Ending'
 import ProjectsSec from '@/Components/Section/ProjectsSec'
 import { personalData } from '@/Data/PersonalData'
+import { seoMap } from '@/Data/seoMap'
 import { schema } from '@/Data/SeoSchema'
 import Breadcrumb from '@/Snippet/Breadcrumb'
 import Pagewidth from '@/Snippet/Pagewidth'
+import SEO from '@/Snippet/SEO'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Head from 'next/head'
@@ -14,19 +16,12 @@ import React from 'react'
 const index = () => {
     return (
         <>
-            <Head>
-                <title>{personalData.name}</title>
-                <meta property="og:title" content={personalData.name} key="title" />
-                <meta name="description" key="description" content={personalData.meta} />
-                <meta name="keywords" content={personalData.tags} />
-                <Analytics />
-                <SpeedInsights />
-            </Head>
+            <SEO path="projects" data={seoMap.projects} />
             <JsonLd data={schema.home} />
             <Layout>
                 <Pagewidth>
                     <Breadcrumb />
-                    <ProjectsSec heading='Projects I Have Done' />
+                    <ProjectsSec heading='Projects With Case Studies' />
                     <Ending />
                 </Pagewidth>
             </Layout>

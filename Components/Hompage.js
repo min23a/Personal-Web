68b9297cbaf0b1b674e7a18ba154b84b129b8adc
min23a <minhazuloabedin0611@gmail.com>
2026-01-31 15:08:01 +0100

    location based pages link added to homepage section

--- Components/Hompage.js
+++ Components/Hompage.js
@@ -2,22 +2,22 @@ import React from 'react'
 import HeaderSection from './Section/HeaderSection';
 import Skills from './Section/Skills';
 import AboutMe from './Section/AboutMe';
-import Ending from './Section/Ending';
-import HeroSection from './Section/HeroSection';
 import Pagewidth from '@/Snippet/Pagewidth';
 import UpworkReviews from './Section/UpworkReviews';
 import { upworkReviews } from '@/Data/upworkReviews';
 import ProjectsSec from './Section/ProjectsSec';
+import LocationBased from './Section/LocationBased';
 
 const Hompage = () => {
     return (
         <>
             <Pagewidth>
                 <HeaderSection />
+                <LocationBased />
                 <ProjectsSec />
+                <UpworkReviews cls="bg-black rounded-xl my-2 sm:my-4 lg:my-6 shadow-lg shadow-indigo-500/20 py-8 px-5 animate-on-scroll animate-fade-up text-white" reviews={upworkReviews} />
                 <AboutMe />
                 <Skills />
-                <UpworkReviews cls="bg-black rounded-xl my-2 sm:my-4 lg:my-6 shadow-lg shadow-indigo-500/20 py-8 px-5 animate-on-scroll animate-fade-up text-white" reviews={upworkReviews} />
             </Pagewidth>
         </>
     )
